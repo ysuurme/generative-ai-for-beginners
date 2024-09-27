05c1ec01c0b305075b7e38e0f0a7f7f674789bf5
YannicSuurmeijer-XPS <44139032+ysuurme@users.noreply.github.com>
2024-09-27 18:18:08 +0200

    created function prompt recipies

--- 06-text-generation-apps/python/aoai-app-recipe.py
+++ 06-text-generation-apps/python/aoai-app-recipe.py
@@ -5,40 +5,39 @@ import dotenv
 # import dotenv
 dotenv.load_dotenv()
 
+# test dotenv
+def dotenv_test():
+    test = {'azure_endpoint': os.environ['AZURE_OPENAI_ENDPOINT'],
+            'model_version': os.environ['AZURE_OPENAI_API_VERSION'],
+            'model_deployment': os.environ['AZURE_OPENAI_DEPLOYMENT']}
+    for key, value in test.items():
+        print(key, value)
+
+
+dotenv_test()
+
 # configure Azure OpenAI service client 
 client = AzureOpenAI(
-  azure_endpoint = os.environ["AZURE_OPENAI_ENDPOINT"], 
+  azure_endpoint = os.environ['AZURE_OPENAI_ENDPOINT'], 
   api_key=os.environ['AZURE_OPENAI_API_KEY'],  
-  api_version = "2023-10-01-preview"
+  api_version = os.environ['AZURE_OPENAI_API_VERSION']
   )
 
 deployment=os.environ['AZURE_OPENAI_DEPLOYMENT']
 
-no_recipes = input("No of recipes (for example, 5: ")
+# Prompt for recipies
+def prompt_recipies(n='1'):
+  no_recipes = n
+  ingredients = input("Provide ingredients (ex. salmon olives): ")
 
-ingredients = input("List of ingredients (for example, chicken, potatoes, and carrots: ")
+# create prompt
+  prompt = f"Provide for {no_recipes} dishes with the following ingredients: {ingredients} a brief tasting note and list the recipe."
+  messages = [{"role": "user", "content": prompt}]
 
-filter = input("Filter (for example, vegetarian, vegan, or gluten-free: ")
-
-# interpolate the number of recipes into the prompt an ingredients
-prompt = f"Show me {no_recipes} recipes for a dish with the following ingredients: {ingredients}. Per recipe, list all the ingredients used, no {filter}: "
-messages = [{"role": "user", "content": prompt}]
-
-completion = client.chat.completions.create(model=deployment, messages=messages, max_tokens=600, temperature = 0.1)
-
+  completion = client.chat.completions.create(model=deployment, messages=messages, max_tokens=600, temperature = 0.1)
 
 # print response
-print("Recipes:")
-print(completion.choices[0].message.content)
+  print(f'Response: {completion.choices[0].message.content}')
 
-old_prompt_result = completion.choices[0].message.content
-prompt_shopping = "Produce a shopping list, and please don't include ingredients that I already have at home: "
 
-new_prompt = f"Given ingredients at home {ingredients} and these generated recipes: {old_prompt_result}, {prompt_shopping}"
-messages = [{"role": "user", "content": new_prompt}]
-completion = client.chat.completions.create(model=deployment, messages=messages, max_tokens=600, temperature=0)
-
-# print response
-print("\n=====Shopping list ======= \n")
-print(completion.choices[0].message.content)
-
+prompt_recipies()
